e06c902cdcfcc3b0fbe59be3bf01208ef7f919fd
Jan Kaufmann <jankaufmann@gmx.net>
2025-12-24 12:51:08 +0100

    Update docs and bump version to 0.0.2

--- src/octopost/parsing.py
+++ src/octopost/parsing.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import pandas as pd
 
 def list_time_dirs(path_to_time_dirs):
-    """
+    """Lists all time directories in a given path and returns them
     
     Parameters
     ----------
@@ -63,9 +63,58 @@ def parse_of(file_name,names=None,usecols=None):
 
     return df
 
-
 def dummy_columns(n=99):
+    """Generates a list of dummy column names for OpenFOAM data files.
+    
+    Parameters
+    ----------
+    n : int, optional
+        Number of dummy columns to generate (excluding the 'time' column). Default is 99.
+        
+    Returns
+    -------
+    names : list of str
+        List of dummy column names, starting with 'time' followed by 'c0', '
+        c1', ..., 'c{n-1}'.
+    """
     
     names = ['time'] + ['c' + str(x) for x in range(n)]
     
     return names 
+
+def filter_time_and_columns(df, time_start=None,time_end=None, data_subset=None):
+    """Filtering time and sub-selecting data columns.
+    
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        The input data frame containing a 'time' column.
+    time_start : float, optional
+        The starting time to filter the data. Default is None, which means no lower limit.
+    time_end : float, optional
+        The ending time to filter the data. Default is None, which means no upper limit.
+    data_subset : list, optional
+        List of column names to select from the data frame. Default is None, which selects all
+        columns.
+        
+    Returns
+    -------
+    pandas.DataFrame
+        The prepared data frame with filtered time and selected columns.
+    """
+    
+    if data_subset is None:
+        data_subset = []
+
+    if time_start is not None:
+        df = df.loc[df.time >= time_start]
+        
+    if time_end is not None:
+        df = df.loc[df.time <= time_end]
+
+    df.set_index('time', drop=True, inplace=True)
+
+    if len(data_subset) > 0:
+        df = df[df.columns.intersection(data_subset)]
+
+    return df
