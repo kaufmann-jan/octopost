b83a3eb73692f358a1d7bc35635943952ec361c1
Jan Kaufmann <kaufmann@dst-org.de>
2026-02-18 08:09:24 +0100

    Re-export convenience functions from package root; bump to 0.0.5

--- src/octopost/__init__.py
+++ src/octopost/__init__.py
@@ -1,1 +1,21 @@
-__version__ = "0.0.4"
+from .reader import (
+    actuatorDisk,
+    forces,
+    residuals,
+    rigidBodyState,
+    sectionalForces,
+    time,
+    waveBuoy,
+)
+
+__version__ = "0.0.5"
+
+__all__ = [
+    "forces",
+    "residuals",
+    "sectionalForces",
+    "rigidBodyState",
+    "time",
+    "actuatorDisk",
+    "waveBuoy",
+]
